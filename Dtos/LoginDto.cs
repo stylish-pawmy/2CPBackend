56b09526a063850285f96b04ace77abc2cd5cde7
Mahdi Bendou <ambesi2026@gmail.com>
2023-03-26 14:40:44 +0100

    Add Login Dto

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+namespace _2cpbackend.Dtos;
+
+using System.ComponentModel.DataAnnotations;
+
+public class LoginDto
+{
+    //Identifier
+    [Required] public string Identifier { get; set; } = null!;
+
+    //Password
+    [Required][DataType(DataType.Password)] public string Password { get; set; } = null!;
+}
