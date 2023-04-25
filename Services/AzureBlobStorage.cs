fb3792ccfd2d4c9f05d6e824f629c9bcfcef0b80
stylish-pawmy <ambesi2026@gmail.com>
2023-04-26 00:49:15 +0100

    Add required services for Azure store

---
+++ Services/AzureBlobStorage.cs
@@ -0,0 +1,41 @@
+namespace _2cpbackend.Services;
+
+using Azure.Storage.Blobs;
+
+public class AzureBlobStorage : IBlobStorage
+{
+    private readonly IConfiguration _configuration;
+
+    public AzureBlobStorage(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<string> UploadBlobAsync(string containerName, string fileName, IFormFile file)
+    {
+        BlobContainerClient container = new BlobContainerClient(
+                _configuration.GetConnectionString("azureStore"),
+                containerName
+            );
+
+        BlobClient blob = container.GetBlobClient(fileName);
+
+        using (Stream stream = file.OpenReadStream())
+        {
+            await blob.UploadAsync(stream);
+        }
+        
+        return blob.Uri.AbsoluteUri;
+    }
+
+    public async Task DeleteBlobAsync(string containerName, string fileName)
+    {
+        BlobContainerClient container = new BlobContainerClient(
+            _configuration.GetConnectionString("azureStore"),
+            containerName);
+        
+        BlobClient blob = container.GetBlobClient(fileName);
+
+        await blob.DeleteIfExistsAsync();
+    }
+}
