d40bb1e075b1f9fa0abbdbc352e206492b2abe05
stylish-pawmy <ambesi2026@gmail.com>
2023-05-01 11:59:10 +0100

    Setup search engine class utility

---
+++ Utilities/SearchEngine.cs
@@ -0,0 +1,19 @@
+namespace _2cpbackend.Utilities;
+
+//Lucine search engine library references
+using Lucene.Net;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Store;
+using Lucene.Net.Search;
+using Lucene.Net.Index;
+using Lucene.Net.Documents;
+using Lucene.Net.Util;
+
+using Microsoft.EntityFrameworkCore;
+
+using _2cpbackend.Models;
+using _2cpbackend.Data;
+
+public class SearchEngine
+{
+}
